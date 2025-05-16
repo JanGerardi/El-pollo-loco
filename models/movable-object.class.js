08d989296f753969854a981ed55468b7db2ca25f
Jan Gerardi <jan_gerardi@outlook.com>
2025-05-16 22:46:55 +0200

    update

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -2,6 +2,13 @@ class MovableObject{
     x;
     y;
     img;
+    height;
+    width;
+
+    loadImage(path){
+        this.img = new Image();
+        this.img.scr = path;
+    }
 
     moveRight(){
         console.log("moving right");
